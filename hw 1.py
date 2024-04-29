9604650aa1a5e7a58e59fd114dfe8595a0b55250
kirilliak <164033245+kirilliak@users.noreply.github.com>
2024-04-29 15:42:47 +0300

    homework 1

---
+++ hw 1.py
@@ -0,0 +1,22 @@
+def total_salary(path):
+    total_salary = 0
+    num_developers = 0
+    
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                name, salary = line.strip().split(',')
+                total_salary += int(salary)
+                num_developers += 1
+            average_salary = total_salary / num_developers
+            
+            return total_salary, average_salary
+        
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+    return 0, 0
+total, average = total_salary("path/to/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
